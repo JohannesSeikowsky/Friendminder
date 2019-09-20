f758d1901e8925e1373fb1c8f0d3f815e71d9904
JohannesSeikowsky <joseikowsky@googlemail.com>
2019-09-20 18:29:03 +0100

    starting to add tests

--- configs.py
+++ configs.py
@@ -2,5 +2,5 @@ import os
 
 gmail_account = os.environ["EMAIL_ACC"]
 gmail_pw = os.environ["EMAIL_PW"]
-path_to_code = os.environ["FRIENDMINDER_PATH"]
-spreadsheet_name = os.environ["FRIENDMINDER_SHEET"]
+path_to_code = "/home/johannes/code/friendminder/"
+spreadsheet_name = "Friends"
---
+++ tests/test_main.py
@@ -0,0 +1,29 @@
+# from friendminder import main
+import sys
+sys.path.append('/home/johannes/code/friendminder')
+
+import unittest, datetime, random
+import main
+
+
+class FriendminderTests(unittest.TestCase):
+	""" some basic tests """
+
+	def test_is_today(self):
+		# checking is_today for today
+		today = datetime.datetime.today()
+		self.assertTrue(main.is_today(today))
+
+		# checking fis_today or future dates
+		interval = random.randint(0, 10)
+		future_date = today + datetime.timedelta(days=interval)
+		self.assertFalse(main.is_today(future_date))
+
+		# checking is_today for past dates
+		interval = random.randint(0, 10)
+		past_date = today - datetime.timedelta(days=interval)
+		self.assertFalse(main.is_today(past_date))
+
+
+if __name__ == "__main__":
+	unittest.main()
